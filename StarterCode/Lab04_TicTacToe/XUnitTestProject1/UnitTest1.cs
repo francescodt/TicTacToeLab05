b10565833f1e9b37e9cdcc76baf62f9bc1317988
Francesco <francescodtroiano@gmail.com>
2020-05-07 16:47:59 -0500

    tests added

---
+++ StarterCode/Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace Tic_Tac_Toe_Tests
+{
+    public class UnitTest1
+    {
+
+        /// <summary>
+        /// If the board changes correctly
+        /// that we can't rewrite or resubmit choices
+        /// if the position doesn't exist
+        /// winner / draw - program stop
+        /// switching players
+        /// </summary>
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
